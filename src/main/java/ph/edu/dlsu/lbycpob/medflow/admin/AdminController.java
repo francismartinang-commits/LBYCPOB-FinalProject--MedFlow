115d64823e84decb4f7c5bbd8dffa4d0d893f5f7
Nash Paragas <nash_paragas@dlsu.edu.ph>
2026-08-08 23:27:47 +0800

    Create admin dashboard controller and view

---
+++ src/main/java/ph/edu/dlsu/lbycpob/medflow/admin/AdminController.java
@@ -0,0 +1,25 @@
+package ph.edu.dlsu.lbycpob.medflow.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String showAdminDashboard(Model model) {
+
+        model.addAttribute(
+                "pageTitle",
+                "Admin Dashboard"
+        );
+
+        model.addAttribute(
+                "pageSubtitle",
+                "Manage accounts, doctor categories, and laboratory sections."
+        );
+
+        return "admin-dashboard";
+    }
+}
