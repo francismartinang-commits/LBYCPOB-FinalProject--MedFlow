d596edc7e202ddc6717aa6846bc2fa21b8a45d0f
Joshua <joshua_balingit@dlsu.edu.ph>
2026-08-09 21:51:57 +0800

    I created the StatusTrackerView class structure and imported necessary libraries.

--- src/main/java/com/dlsu/medflow/components/StatusTrackerView.java
+++ src/main/java/com/dlsu/medflow/components/StatusTrackerView.java
@@ -1,4 +1,21 @@
-package com.dlsu.medflow.components;
+package com.dlsu.medflow.gui.components;
 
-public class StatusTrackerView {
+import com.dlsu.medflow.model.Visit;
+import com.dlsu.medflow.model.VisitStatus;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class StatusTrackerView extends VBox {
+
 }
