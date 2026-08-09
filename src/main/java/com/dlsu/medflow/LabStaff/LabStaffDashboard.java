e71d64b2945d7fc663ddf369cc386b16da0e4567
Joshua <joshua_balingit@dlsu.edu.ph>
2026-08-09 22:33:47 +0800

    I set up the package name and imported JavaFX and model dependencies.

---
+++ src/main/java/com/dlsu/medflow/LabStaff/LabStaffDashboard.java
@@ -0,0 +1,15 @@
+package com.dlsu.medflow.gui.labstaff;
+
+import com.dlsu.medflow.gui.components.UI;
+import com.dlsu.medflow.model.*;
+import com.dlsu.medflow.service.HospitalDataStore;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
