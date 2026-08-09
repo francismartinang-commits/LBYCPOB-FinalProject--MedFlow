53f8efe04e75a0ed527933b27bbaec94cc5418f9
Nash Paragas <nash_paragas@dlsu.edu.ph>
2026-08-09 23:12:27 +0800

    Add nurse dashboard dependencies

---
+++ src/main/java/com/dlsu/medflow/gui/nurse/NurseDashboard.java
@@ -0,0 +1,22 @@
+package com.dlsu.medflow.gui.nurse;
+
+import com.dlsu.medflow.gui.components.UI;
+import com.dlsu.medflow.gui.patient.PatientRegistrationDialog;
+import com.dlsu.medflow.model.*;
+import com.dlsu.medflow.service.HospitalDataStore;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
+
+public class NurseDashboard extends VBox {
+
+}
