15537317ff2bade1f0963ddd3bf20ab49eed0bff
Nash Paragas <nash_paragas@dlsu.edu.ph>
2026-08-12 00:53:30 +0800

    Added Mainshell for patient and imports

---
+++ src/main/java/com/dlsu/medflow/gui/patient/MainShell.java
@@ -0,0 +1,22 @@
+package com.dlsu.medflow.gui;
+
+import com.dlsu.medflow.gui.components.UI;
+import com.dlsu.medflow.model.User;
+import com.dlsu.medflow.service.HospitalDataStore;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class MainShell {
+}
