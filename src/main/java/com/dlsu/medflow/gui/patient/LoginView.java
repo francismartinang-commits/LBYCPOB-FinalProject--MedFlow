3778821213a5c73755e4ec5ce85388e68eb18017
Nash Paragas <nash_paragas@dlsu.edu.ph>
2026-08-12 00:48:12 +0800

    Added paitent viewlogin and imports

---
+++ src/main/java/com/dlsu/medflow/gui/patient/LoginView.java
@@ -0,0 +1,18 @@
+package com.dlsu.medflow.gui.patient;
+
+import com.dlsu.medflow.gui.components.UI;
+import com.dlsu.medflow.gui.patient.PatientRegistrationDialog;
+import com.dlsu.medflow.model.User;
+import com.dlsu.medflow.service.HospitalDataStore;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polyline;
+
+import java.util.function.Consumer;
+
+public class LoginView {
+}
