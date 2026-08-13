cb45488f92f6a78833c2933abebd98e700c59ff2
Nash Paragas <nash_paragas@dlsu.edu.ph>
2026-08-13 22:46:53 +0800

    Add Doctor model dependencies

--- src/main/java/com/dlsu/medflow/model/Doctor.java
+++ src/main/java/com/dlsu/medflow/model/Doctor.java
@@ -1,4 +1,13 @@
 package com.dlsu.medflow.model;
 
+import com.dlsu.medflow.gui.doctor.DoctorDashboard;
+import com.dlsu.medflow.service.HospitalDataStore;
+import com.dlsu.medflow.service.LabRoutingEngine;
+import javafx.scene.Parent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Doctor {
 }
