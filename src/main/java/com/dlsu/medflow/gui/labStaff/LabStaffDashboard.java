7082fc27645989d81c6d6c3691906d0b5d9a1c46
Nash Paragas <nash_paragas@dlsu.edu.ph>
2026-08-13 22:44:10 +0800

    Add Admin model dashboard dependencies

--- src/main/java/com/dlsu/medflow/gui/labStaff/LabStaffDashboard.java
+++ src/main/java/com/dlsu/medflow/gui/labStaff/LabStaffDashboard.java
@@ -1,4 +1,4 @@
-package com.dlsu.medflow.gui.labstaff;
+package com.dlsu.medflow.gui.labStaff;
 
 import com.dlsu.medflow.gui.components.UI;
 import com.dlsu.medflow.model.*;
---
+++ src/main/java/com/dlsu/medflow/model/Admin.java
@@ -0,0 +1,8 @@
+package com.dlsu.medflow.model;
+
+import com.dlsu.medflow.gui.admin.AdminDashboard;
+import com.dlsu.medflow.service.HospitalDataStore;
+import javafx.scene.Parent;
+
+public class Admin {
+}
