e72a5bda18908ef5f7fe78ab0c3c973a9e305364
Joshua <joshua_balingit@dlsu.edu.ph>
2026-08-16 20:07:48 +0800

    Declare Nurse class inheriting from User

--- src/main/java/com/dlsu/medflow/model/Nurse.java
+++ src/main/java/com/dlsu/medflow/model/Nurse.java
@@ -4,3 +4,9 @@ import com.dlsu.medflow.service.HospitalDataStore;
 
 import java.util.HashMap;
 import java.util.Map;
+
+/**
+ * INHERITANCE: child class of {@link User}. "Nurse/Staff - can confirm
+ * registration and sample collection" (System Framework).
+ */
+public class Nurse extends User {
