1516683a1169f92529f4d075f1372509724896ad
Joshua <joshua_balingit@dlsu.edu.ph>
2026-08-16 19:37:51 +0800

    Declare LabStaff class extending User

--- src/main/java/com/dlsu/medflow/model/LabStaff.java
+++ src/main/java/com/dlsu/medflow/model/LabStaff.java
@@ -4,3 +4,9 @@ import com.dlsu.medflow.service.HospitalDataStore;
 
 import java.util.HashMap;
 import java.util.Map;
+
+/**
+ * INHERITANCE: child class of {@link User}. "Laboratory Staff - can view lab
+ * requests and encode findings" (System Framework).
+ */
+public class LabStaff extends User {
