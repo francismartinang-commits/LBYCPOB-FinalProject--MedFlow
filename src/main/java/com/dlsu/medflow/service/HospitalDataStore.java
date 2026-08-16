efd817c4c874be71e792887a89f094b974566abc
Tin <francis_martin_ang@dlsu.edu.ph>
2026-08-16 09:30:51 +0800

    added completed the static method

--- src/main/java/com/dlsu/medflow/service/HospitalDataStore.java
+++ src/main/java/com/dlsu/medflow/service/HospitalDataStore.java
@@ -45,6 +45,9 @@ public static HospitalDataStore loadOrCreate() {
          if (loaded instanceof HospitalDataStore) {
             return (HospitalDataStore) loaded;
          }
+      } catch (IOException | ClassNotFoundException ex) {
+         System.err.println("Could not read saved data, starting fresh: " + ex.getMessage());
+      }a
 
 
 
