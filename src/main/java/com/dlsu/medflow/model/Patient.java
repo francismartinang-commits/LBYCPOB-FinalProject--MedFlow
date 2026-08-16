d1d755f776d14fcbc7963802d53dc5b77952afd4
Joshua <joshua_balingit@dlsu.edu.ph>
2026-08-16 20:22:56 +0800

    Implement getters and setters for age and gender fields

--- src/main/java/com/dlsu/medflow/model/Patient.java
+++ src/main/java/com/dlsu/medflow/model/Patient.java
@@ -69,4 +69,20 @@ public class Patient extends User {
     public List<Visit> getVisitHistory() {
         return visitHistory;
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
