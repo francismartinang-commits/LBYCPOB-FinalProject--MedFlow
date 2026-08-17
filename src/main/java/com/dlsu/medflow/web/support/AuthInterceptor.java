1f52cc1dc92d182b992c24145700ae3cdcff5cc4
Tin <francis_martin_ang@dlsu.edu.ph>
2026-08-17 16:44:06 +0800

    added all needed imports

--- src/main/java/com/dlsu/medflow/web/support/AuthInterceptor.java
+++ src/main/java/com/dlsu/medflow/web/support/AuthInterceptor.java
@@ -1,4 +1,8 @@
 package com.dlsu.medflow.web.support;
 
-public class AuthInterceptor {
-}
+import com.dlsu.medflow.model.Role;
+import com.dlsu.medflow.model.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
