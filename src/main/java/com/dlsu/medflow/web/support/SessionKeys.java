8a3817372cc946acfa206c7ba313ce035b982c8b
Tin <francis_martin_ang@dlsu.edu.ph>
2026-08-17 17:09:42 +0800

    added the asession keys class with private field and containing a public field for CURRENT_USER

--- src/main/java/com/dlsu/medflow/web/support/SessionKeys.java
+++ src/main/java/com/dlsu/medflow/web/support/SessionKeys.java
@@ -1,4 +1,10 @@
 package com.dlsu.medflow.web.support;
 
-public class SessionKeys {
+/** Names of attributes stored in the HTTP session. Kept in one place to avoid typo'd string literals. */
+public final class SessionKeys {
+
+    public static final String CURRENT_USER = "currentUser";
+
+    private SessionKeys() {
+    }
 }
