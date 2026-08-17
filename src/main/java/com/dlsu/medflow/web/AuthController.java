ba51d5e22310e1afcb3b59f63b1c3fef88814c05
Tin <francis_martin_ang@dlsu.edu.ph>
2026-08-17 17:23:43 +0800

    created AuthController class and added all imports

---
+++ src/main/java/com/dlsu/medflow/web/AuthController.java
@@ -0,0 +1,20 @@
+package com.dlsu.medflow.web;
+
+import com.dlsu.medflow.model.Patient;
+import com.dlsu.medflow.model.User;
+import com.dlsu.medflow.service.HospitalDataStore;
+import com.dlsu.medflow.web.support.SessionKeys;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class AuthController {
+}
